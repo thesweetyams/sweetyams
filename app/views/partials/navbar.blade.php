82ea2c1bd73753057a9cd58e2b5c4130f484d256
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-04-25 10:31:27 -0500

    Edited all .scss files 	-changed to .sass 	-deleted ';' and '{}'
    Modified navbar.blade.php
    	-changed to use an <ul>

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,22 +1,43 @@
-<nav id="">
-    <div class="">
-        <div class="">
-            <a href="" class="">Sweet Yams</a>
-        </div>
-        <div class="">
-            <a href="" class=""><span>Menu</span><span class=""></span></a>
-        </div>
-        <div class="">
-            <a href="" class=""><span>Our Story</span><span class="arr"></span></a>
-        </div>
-        <div class="">
-            <a href="" class=""><span></span></a>
-        </div>
-        <div class="">
-            <a href="" class=""><span>Location</span><span class=""></span></a>
-        </div>
-        <div class="">
-            <a href="" class=""><span>Contact Us</span><span class=""></span></a>
-        </div>
-    </div>
+<nav id="navbar">
+  <ul class="navList">
+    <li class="navList__item">Sweet Yams</li>
+    <li class="navList__item">Menu</li>
+    <li class="navList__item--logo">Logo</li>
+    <li class="navList__item">About Us</li>
+    <li class="navList__item">Community</li>
+  </ul>
+
+
 </nav><!--end #main-nav-->
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <div class="">
+<div class="">
+<a href="" class="">Sweet Yams</a>
+</div>
+<div class="">
+<a href="" class=""><span>Menu</span><span class=""></span></a>
+</div>
+<div class="">
+<a href="" class=""><span>Our Story</span><span class="arr"></span></a>
+</div>
+<div class="">
+<a href="" class=""><span></span></a>
+</div>
+<div class="">
+<a href="" class=""><span>Location</span><span class=""></span></a>
+</div>
+<div class="">
+<a href="" class=""><span>Contact Us</span><span class=""></span></a>
+</div>
+</div> --}}
