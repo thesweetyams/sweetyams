bf465e4a210865cc0d98a62428f9864105e028d4
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-04-29 15:55:39 -0500

    Awesome dropdowns!

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="nav-container">
   <nav class="flex-nav">
     <ul class="flex-ul">
-      <li class="flex-ul__item"><div class="flex-ul__item--hover" href="#">Menu</div>
+      <li class="flex-ul__item"><div class="flex-ul__item--hover">Menu</div>
         <div class="dropdown">
           <ul class="dropdown-list">
             <li><a href="#">View Menu</a></li>
@@ -9,21 +9,22 @@
             <li><a href="#">Catering</a></li>
             <li><a href="#">Order Now!</a></li>
           </ul>
+          <div class="box-shadow"></div>
         </div>
       </li>
       <li class="flex-ul__item"><a class="flex-ul__item--hover" href="#">Location</a></li>
       <li class="flex-ul__item"><div class="logo__box"><a href="#"><img id="logo" src="../../img/navicons/logo.png"/></a></div></li>
       <li class="flex-ul__item"><a class="flex-ul__item--hover" href="#">About</a></li>
-      <li class="flex-ul__item"><a class="flex-ul__item--hover"href="#">Community</a>
-        {{-- <div class="flex-ul__item">
-          <div class="flex-ul__item__dropdown">
-            <a href="/projects">Projects</a>
-            <a href="/events">Events</a>
-            <a href="#">Edit Post</a>
-          </div>
-        </div> --}}
+      <li class="flex-ul__item"><div class="flex-ul__item--hover">Community</div>
+        <div class="dropdown" id="community-dropdown">
+          <ul class="dropdown-list" id="community-dropdown-list">
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Events</a></li>
+          </ul>
+          <div class="box-shadow"></div>
+        </div>
       </li>
-      {{-- <li class="flex-ul__item--social"><a href="#"><img src="../../img/logo/facebook.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/instagram.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/yelp.png" class="logo__image--yelp"/></a></li> --}}
     </ul>
   </nav>
 </div>
