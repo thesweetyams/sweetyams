06c1e75dbcc2b2e1560f031bcbe4c616a0ea894c
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-04-29 11:56:10 -0500

    Fixed responsiveness on navbar Added dropdowns!!!!

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,28 +1,32 @@
-<nav class="flex-nav">
-  <ul class="flex-ul">
-    <li class="flex-ul__item"><p><a href="#" class="dropdown">Menu</a></p>
-      <div class="flex-ul__item">
-        <div class="flex-ul__item__dropdown">
-          <a href="/menu">View Menu</a>
-          <a href="/juice">Juice Subscriptions</a>
-          <a href="/catering">Catering</a>
-          <a href="/order">Order Now</a>
+<div class="nav-container">
+  <nav class="flex-nav">
+    <ul class="flex-ul">
+      <li class="flex-ul__item"><div class="flex-ul__item--hover" href="#">Menu</div>
+        <div class="dropdown">
+          <ul class="dropdown-list">
+            <li><a href="#">View Menu</a></li>
+            <li><a href="#">Juice Subscription</a></li>
+            <li><a href="#">Catering</a></li>
+            <li><a href="#">Order Now!</a></li>
+          </ul>
         </div>
-      </div></li>
-    <li class="flex-ul__item"><p><a href="#">Location</a></p></li>
-    <li class="flex-ul__item"><p><a href="#"><img id="logo" class="dropdown__arrow" src="../../img/navicons/logo.png" alt="" /></a></p></li>
-    <li class="flex-ul__item"><p><a href="#">About Us</a></p></li>
-    <li class="flex-ul__item"><p><a href="#">Community</a></p>
-      <div class="flex-ul__item">
-        <div class="flex-ul__item__dropdown">
-          <a href="/projects">Projects</a>
-          <a href="/events">Events</a>
-          <a href="#">Edit Post</a>
-        </div>
-      </div></li>
-    {{-- <li class="flex-ul__item--social"><a href="#"><img src="../../img/logo/facebook.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/instagram.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/yelp.png" class="logo__image--yelp"/></a></li> --}}
-  </ul>
-</nav>
+      </li>
+      <li class="flex-ul__item"><a class="flex-ul__item--hover" href="#">Location</a></li>
+      <li class="flex-ul__item"><div class="logo__box"><a href="#"><img id="logo" src="../../img/navicons/logo.png"/></a></div></li>
+      <li class="flex-ul__item"><a class="flex-ul__item--hover" href="#">About</a></li>
+      <li class="flex-ul__item"><a class="flex-ul__item--hover"href="#">Community</a>
+        {{-- <div class="flex-ul__item">
+          <div class="flex-ul__item__dropdown">
+            <a href="/projects">Projects</a>
+            <a href="/events">Events</a>
+            <a href="#">Edit Post</a>
+          </div>
+        </div> --}}
+      </li>
+      {{-- <li class="flex-ul__item--social"><a href="#"><img src="../../img/logo/facebook.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/instagram.png" class="logo__image"/></a><a href="#"><img src="../../img/logo/yelp.png" class="logo__image--yelp"/></a></li> --}}
+    </ul>
+  </nav>
+</div>
 
 
 
