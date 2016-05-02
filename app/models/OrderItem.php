045c0abfbca8ddfb3bfbb88ec5bc22e5da599397
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-05-02 11:44:00 -0500

    Added favicon!!! HUUUUUUGGGEEEE UPDATE SUPER COOL MUST HAVE MUCH WOW SUCH ICON

--- app/models/OrderItem.php
+++ app/models/OrderItem.php
@@ -5,28 +5,28 @@ class OrderItem extends Eloquent
 	protected $table = 'order_items';
 	public $timestamps = false;
 
-	public function menuItems() 
+	public function menuItems()
 	{
 		return $this->hasOne('MenuItem');
 	}
 	public function subtotal($id)
 	{
 		$total = 0;
-		$items = OrderItem::->where('item_id', $id)->get();
+		// $items = OrderItem::->where('item_id', $id)->get();
 
 		foreach($items as $item) {
 			$total += $item->menuItems()->price;
 		}
 
-		// do the same thing for add_ons based off the addons 
+		// do the same thing for add_ons based off the addons
 		return $total;
 	}
-	public function orderItemAddOns() 
+	public function orderItemAddOns()
 	{
 		return $this->hasMany('OrderItemAddOn');
 	}
 
-	public function orders() 
+	public function orders()
 	{
 		return $this->belongsTo('Order');
 	}
