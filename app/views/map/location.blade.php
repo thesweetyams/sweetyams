5f384b5b360b3ef0eb967cb986c4d009e7160001
Tomas Leffew <tleffew1994@gmail.com>
2016-05-03 16:23:00 -0500

    fixed navbar on full screen

--- app/views/map/location.blade.php
+++ app/views/map/location.blade.php
@@ -1,9 +1,19 @@
 @extends('.master')
+@section('css')
+<style>
+    #map-canvas{
+        top: 20%; /* IMPORTANT */
+        left: 20%; /* IMPORTANT */
+        width: 750px;
+        height: 417px;
+    }
+
+</style>
+@endsection
 @section('content')
     <div id="map-canvas"></div>
-    <input id="addressForm" type="text" name="name" value="" placeholder="Enter address">
-        </input>
-            <button id="addressButton" type="submit" name="button">Submit</button>
+    <input id="addressForm" type="text" name="name" value="" placeholder="Enter address"></input>
+    <button id="addressButton" type="submit" name="button">Submit</button>
 
 @endsection
 
