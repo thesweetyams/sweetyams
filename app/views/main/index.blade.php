718586c1fade12aab47cadbd3580b1ac153ec268
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-05-09 10:50:50 -0500

    Working on index carousel

--- app/views/main/index.blade.php
+++ app/views/main/index.blade.php
@@ -1,9 +1,22 @@
 @extends('master')
+@section('css')
+	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+@stop
 
 @section('content')
-	
+
 @stop
 
 @section('js')
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="slick/slick.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$('.your-class').slick({
+			setting-name: setting-value
+		});
+	});
 
+	</script>
 @stop
