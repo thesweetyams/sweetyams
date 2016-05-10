6a4b153d75e4e4738b1f968722e249d574422ea9
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-05-10 15:02:06 -0500

    Fixing merge conflict

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,10 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="publishable_key" content="{{$_ENV['STRIPE_PUBLISHABLE_KEY']}}">
-<<<<<<< HEAD
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-=======
->>>>>>> 32bd357a5c555b20f0e18f5b011374e0c9304ce0
 	<title>Sweet Yams</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -20,9 +17,8 @@
 	<div class="wrapper">
 		@include('.partials.navbar')
 	</div>
-	
-	<div class="Site-content">
->>>>>>> 32bd357a5c555b20f0e18f5b011374e0c9304ce0
+
+	<div class="desktop-content">
 		<div class="content-container">
 			@yield('content')
 		</div>
@@ -31,6 +27,6 @@
 	@include('.partials.footer')
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-  	<script src="/js/main.js"></script>
+  <script src="/js/main.js"></script>
 	@yield('js')
 </body>
