29e403a8db5fe508788c6741026e82d4ec6de517
Penthious <tleffew1994@gmail.com>
2016-05-10 20:34:03 -0500

    fixed confim.blade style

--- app/models/Menu.php
+++ app/models/Menu.php
@@ -1,12 +1,7 @@
 <?php
 
 class Menu extends BaseModel
-{
-  public static $rules = array(
-    'name'   => 'required|min:1',
-    'description' => 'required',
-    'price' => 'required'
-  );
+{  
   protected $table = 'menu';
   public $timestamps = false;
 
